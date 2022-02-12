9759b228e5f08deae07aea401537a990b0573210
jamieday22 <jamierobertday@hotmail.com>
2022-02-12 10:57:03 +0000

    many to many added unsure if working, delete not working now for film and actor error 500

--- src/main/java/com/jamie/demo/model/Actor.java
+++ src/main/java/com/jamie/demo/model/Actor.java
@@ -1,13 +1,24 @@
 package com.jamie.demo.model;
 
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-import lombok.Data;
 
 @Data
 @Entity
@@ -18,11 +29,64 @@ public class Actor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int actor_id;
 
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "first_name") // ,  nullable = false)
     private String first_name;
 
     @Column(name = "last_name")
     private String last_name;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonIgnore
+    @JoinTable(name = "film_actor",
+            joinColumns = { @JoinColumn(name = "actor_id")},
+            inverseJoinColumns = { @JoinColumn (name = "film_id")})
+    private Set<Film> film = new HashSet<>();
 
+
+
+    public Actor(String first_name, String last_name) {
+        super();
+        this.first_name = first_name;
+        this.last_name = last_name;
+
+    }
+
+    public Actor( ) {
+    }
+
+
+    public int getActor_id() {
+        return actor_id;
+    }
+
+    public void setActor_id(int actor_id) {
+        this.actor_id = actor_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Set<Film> getFilm() {
+        return film;
+    }
+
+    public void setFilm(Set<Film> film) {
+        this.film = film;
+    }
 }
+
+
+
--- src/main/java/com/jamie/demo/model/Film.java
+++ src/main/java/com/jamie/demo/model/Film.java
@@ -1,15 +1,20 @@
 package com.jamie.demo.model;
 
-import javax.persistence.Column;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-
-import lombok.Data;
-
-import java.time.Year;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
@@ -19,6 +24,7 @@ public class Film {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int film_id;
+
 
     @Column(name = "title")
     private String title;
@@ -35,10 +41,69 @@ public class Film {
     @Column(name = "rating")
     private String rating;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "film")
+    @JsonIgnore
+    private Set<Actor> actor = new HashSet<>();
 
 
+    public Film(){
+
+    }
+
+    public int getFilm_id() {
+        return film_id;
+    }
+
+    public void setFilm_id(int film_id) {
+        this.film_id = film_id;
+    }
 
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public String getLanguage_id() {
+        return language_id;
+    }
+
+    public void setLanguage_id(String language_id) {
+        this.language_id = language_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(Integer release_year) {
+        this.release_year = release_year;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Set<Actor> getActor() {
+        return actor;
+    }
+
+    public void setActor(Set<Actor> actor) {
+        this.actor = actor;
+    }
 }
