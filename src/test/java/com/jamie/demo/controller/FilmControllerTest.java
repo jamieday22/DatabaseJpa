23fe45691f230541f2ced0e7a929d1ccecb7bbf5
jamieday22 <jamierobertday@hotmail.com>
2022-02-21 22:26:05 +0000

    chanaged mvc and now the tests work without issues only one test needs sorting

--- src/test/java/com/jamie/demo/controller/FilmControllerTest.java
+++ src/test/java/com/jamie/demo/controller/FilmControllerTest.java
@@ -1,90 +1,84 @@
 package com.jamie.demo.controller;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.jamie.demo.model.Actor;
-import com.jamie.demo.service.ActorService;
+import com.jamie.demo.model.Film;
+import com.jamie.demo.service.FilmService;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.mockito.ArgumentMatchers.*;
+
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 
-@WebMvcTest(ActorController.class)
-public class ActorControllerTest {
+@WebMvcTest(FilmController.class)
+public class FilmControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private ActorService actorService;
+    private FilmService filmService;
 
     @Autowired
     private ObjectMapper objectMapper;
 
 //    @Test
-//    public void givenActorObject_whenCreateActor_thenReturnSavedActor() throws Exception{
+//    public void givenFilmObject_whenCreateFilm_thenReturnSavedFilm() throws Exception{
 //
 //        // given - precondition or setup
-//        Actor actor = Actor.builder()
+//        Film film = Film.builder()
 //                .firstName("Dave")
 //                .lastName("Johnson")
 //                .build();
-//        given(actorService.saveActor(any(Actor.class)))
+//        given(filmService.saveFilm(any(Film.class)))
 //                .willAnswer((invocation)-> invocation.getArgument(0));
 //
 //        // when - action or behaviour that we are going test
-//        ResultActions response = mockMvc.perform(post("/api/actor")
+//        ResultActions response = mockMvc.perform(post("/api/film")
 //                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(actor)));
+//                .content(objectMapper.writeValueAsString(film)));
 //
 //        // then - verify the result or output using assert statements
 //        response.andDo(print()).
 //                andExpect(status().isCreated())
 //                .andExpect(jsonPath("$.first_name",
-//                        is(actor.getActor_id())))
+//                        is(film.getFilm_id())))
 //                .andExpect(jsonPath("$.last_name",
-//                        is(actor.getLastName())));
+//                        is(film.getLastName())));
 //
 //
 //    }
 
-    // JUnit test for Get All actors REST API
+    // JUnit test for Get All films REST API
     @Test
-    public void givenListOfActors_whenGetAllActors_thenReturnActorsList() throws Exception{
+    public void givenListOfFilms_whenGetAllFilms_thenReturnFilmsList() throws Exception{
         // given - precondition or setup
-        List<Actor> listOfActors = new ArrayList<>();
-        listOfActors.add(Actor.builder().firstName("Dave").lastName("Johnson").build());
-        listOfActors.add(Actor.builder().firstName("Tony").lastName("Dave").build());
-        given(actorService.getAllActors()).willReturn(listOfActors);
+        List<Film> listOfFilms = new ArrayList<>();
+        listOfFilms.add(Film.builder().Title("New").Description("description").Rating("pg").build());
+        listOfFilms.add(Film.builder().Title("description").Description("New").Rating("pg").build());
+        given(filmService.getAllFilms()).willReturn(listOfFilms);
 
         // when -  action or the behaviour that we are going test
-        ResultActions response = mockMvc.perform(get("/api/actors"));
+        ResultActions response = mockMvc.perform(get("/api/films"));
 
         // then - verify the output
         response.andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(jsonPath("$.size()",
-                        is(listOfActors.size())));
+                        is(listOfFilms.size())));
 
     }
 }
-
-    
