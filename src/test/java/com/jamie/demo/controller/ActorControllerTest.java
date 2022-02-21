73d03d3cc18cd0594de09670b000a7ba1bd809aa
jamieday22 <jamierobertday@hotmail.com>
2022-02-21 22:59:12 +0000

    all the tests are working now need ot add more

--- src/test/java/com/jamie/demo/controller/ActorControllerTest.java
+++ src/test/java/com/jamie/demo/controller/ActorControllerTest.java
@@ -39,32 +39,32 @@ public class ActorControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-//    @Test
-//    public void givenActorObject_whenCreateActor_thenReturnSavedActor() throws Exception{
-//
-//        // given - precondition or setup
-//        Actor actor = Actor.builder()
-//                .firstName("Dave")
-//                .lastName("Johnson")
-//                .build();
-//        given(actorService.saveActor(any(Actor.class)))
-//                .willAnswer((invocation)-> invocation.getArgument(0));
-//
-//        // when - action or behaviour that we are going test
-//        ResultActions response = mockMvc.perform(post("/api/actor")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(actor)));
-//
-//        // then - verify the result or output using assert statements
-//        response.andDo(print()).
-//                andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.first_name",
-//                        is(actor.getActor_id())))
-//                .andExpect(jsonPath("$.last_name",
-//                        is(actor.getLastName())));
-//
-//
-//    }
+    @Test
+    public void givenActorObject_whenCreateActor_thenReturnSavedActor() throws Exception{
+
+        // given - precondition or setup
+        Actor actor = Actor.builder()
+                .firstName("Ramesh")
+                .lastName("Fadatare")
+                .build();
+        given(actorService.saveActor(any(Actor.class)))
+                .willAnswer((invocation)-> invocation.getArgument(0));
+
+        // when - action or behaviour that we are going test
+        ResultActions response = mockMvc.perform(post("/api/actors")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(actor)));
+
+        // then - verify the result or output using assert statements
+        response.andDo(print()).
+                andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName",
+                        is(actor.getFirstName())))
+                .andExpect(jsonPath("$.lastName",
+                        is(actor.getLastName())));
+
+
+    }
 
     // JUnit test for Get All actors REST API
     @Test
@@ -85,6 +85,126 @@ public class ActorControllerTest {
                         is(listOfActors.size())));
 
     }
+
+    // positive scenario - valid actor id
+    // JUnit test for GET actor by id REST API
+    @Test
+    public void givenActorId_whenGetActorById_thenReturnActorObject() throws Exception{
+        // given - precondition or setup
+        int actor_id = 1;
+        Actor actor = Actor.builder()
+                .firstName("Ramesh")
+                .lastName("Fadatare")
+                .build();
+        given(actorService.getActorById(actor_id)).willReturn(Optional.of(actor));
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(get("/api/actors/{actor_id}", actor_id));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(actor.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(actor.getLastName())));
+
+
+    }
+
+    // negative scenario - valid actor id
+    // JUnit test for GET actor by id REST API
+    @Test
+    public void givenInvalidActorId_whenGetActorById_thenReturnEmpty() throws Exception{
+        // given - precondition or setup
+        int actor_id = 1;
+        Actor actor = Actor.builder()
+                .firstName("Ramesh")
+                .lastName("Fadatare")
+                .build();
+        given(actorService.getActorById(actor_id)).willReturn(Optional.empty());
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(get("/api/actors/{actor_id}", actor_id));
+
+        // then - verify the output
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+
+    }
+    // JUnit test for update actor REST API - positive scenario
+    @Test
+    public void givenUpdatedActor_whenUpdateActor_thenReturnUpdateActorObject() throws Exception{
+        // given - precondition or setup
+        int actor_id = 1;
+        Actor savedActor = Actor.builder()
+                .firstName("Ramesh")
+                .lastName("Fadatare")
+                .build();
+
+        Actor updatedActor = Actor.builder()
+                .firstName("Ram")
+                .lastName("Jadhav")
+                .build();
+        given(actorService.getActorById(actor_id)).willReturn(Optional.of(savedActor));
+        given(actorService.updateActor(any(Actor.class)))
+                .willAnswer((invocation)-> invocation.getArgument(0));
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/api/actors/{actor_id}", actor_id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedActor)));
+
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(updatedActor.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(updatedActor.getLastName())));
+
+    }
+
+    // JUnit test for update actor REST API - negative scenario
+    @Test
+    public void givenUpdatedActor_whenUpdateActor_thenReturn404() throws Exception{
+        // given - precondition or setup
+        int actor_id = 1;
+        Actor savedActor = Actor.builder()
+                .firstName("Ramesh")
+                .lastName("Fadatare")
+                .build();
+
+        Actor updatedActor = Actor.builder()
+                .firstName("Ram")
+                .lastName("Jadhav")
+                .build();
+        given(actorService.getActorById(actor_id)).willReturn(Optional.empty());
+        given(actorService.updateActor(any(Actor.class)))
+                .willAnswer((invocation)-> invocation.getArgument(0));
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/api/actors/{actor_id}", actor_id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedActor)));
+
+        // then - verify the output
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    // JUnit test for delete actor REST API
+    @Test
+    public void givenActorId_whenDeleteActor_thenReturn200() throws Exception{
+        // given - precondition or setup
+        int actor_id = 1;
+        willDoNothing().given(actorService).deleteActor(actor_id);
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(delete("/api/actors/{actor_id}", actor_id));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print());
+    }
 }
+
 
     
