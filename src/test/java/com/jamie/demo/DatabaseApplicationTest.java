5196d94dfb259ee7e77fba7a383c78a8e88d1b45
jamieday22 <jamierobertday@hotmail.com>
2022-02-22 22:14:53 +0000

    added database app test all should be ok only need exception test the complete

---
+++ src/test/java/com/jamie/demo/DatabaseApplicationTest.java
@@ -0,0 +1,18 @@
+package com.jamie.demo;
+
+import org.junit.jupiter.api.Test;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DatabaseApplicationTest.class)
+public class DatabaseApplicationTest {
+
+    @Test
+    public void DatabaseMainTest() {
+    }
+}
+
